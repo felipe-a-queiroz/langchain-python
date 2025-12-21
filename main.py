46079aa7706bfc2a7a0124df7cea3b447feb47f8
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2025-12-21 17:30:46 -0300

    Definindo a resposta do modelo como um objeto JSON

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+# Importa ChatOpenAI do langchain_openai para interagir com modelos de linguagem
 from langchain_openai import ChatOpenAI
+# Importa PromptTemplate do langchain_core para criar templates de prompts
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+# Importa JsonOutputParser e StrOutputParser do langchain_core para processar saídas do modelo
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+# Importa Field do Pydantic para definir campos em modelos de dados
+# Importa BaseModel do Pydantic para criar modelos de dados estruturados
+from pydantic import Field, BaseModel
+# Importa set_debug do langchain_core.globals para configurar o modo de depuração
+from langchain_core.globals import set_debug
+
+# Ativando o modo de depuração para obter mais informações durante a execução
+set_debug(True)
+
+# Definindo o modelo de dados para a resposta esperada
+class Destino(BaseModel):
+    cidade: str = Field(description="Nome da cidade sugerida")
+    motivo: str = Field(description="Motivo pelo qual a cidade foi sugerida")
+
+# Definindo o parser de saída para converter a resposta do modelo em um objeto Pydantic
+parseador = JsonOutputParser(pydantic_object=Destino)
 
 # Definindo o prompt para sugerir uma cidade com base no interesse do usuário
 prompt_cidade = PromptTemplate(
     template="""
         Sugira uma cidade dado o meu interesse em {interesse}.
+        {formato_de_saida}
     """,
-    input_variables=["interesse"]
-
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()},
 )
 
 # Definindo o interesse do usuário
-interesse = "tecnologia e inovação"
+interesse = "catolicismo e arquitetura gótica europeia"
 
 # Configurando o modelo de linguagem local
 modelo = ChatOpenAI(
@@ -22,8 +42,9 @@ modelo = ChatOpenAI(
     base_url="http://127.0.0.1:1234/v1"
 )
 
+
 # Criando a cadeia de processamento com LCEL
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 # Invocando a cadeia com o interesse do usuário
 resposta = cadeia.invoke(
