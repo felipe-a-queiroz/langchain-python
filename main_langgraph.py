d6714c09e345c78f66acec095987ecc45416fc21
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2025-12-22 12:06:51 -0300

    Orquestrando assistentes sem LangGraph

--- main_langgraph.py
+++ main_langgraph.py
@@ -8,6 +8,9 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 # Importando os para acessar variáveis de ambiente
 import os
+# Importando Literal e TypeDict para definir tipos literais em modelos de dados
+from typing import Literal, TypedDict
+
 
 load_dotenv()
 api_key = os.environ.get("GOOGLE_API_KEY")
@@ -27,7 +30,45 @@ prompt_consultor = ChatPromptTemplate.from_messages(
     ],
 )
 
-# Combinando o prompt com o modelo e o parser de saída
-assistente = prompt_consultor | modelo | StrOutputParser()
+# Definindo o prompt para o consultor de praia
+prompt_consultor_praia = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Apresente-se como Sra. Praia. Você é um consultor de viagens especialista em destinos de praia"),
+        ("human", "{query}"),
+    ],
+)
 
-print(assistente.invoke({"query": "Quais são os melhores destinos para visitar na Europa?"}))
+# Definindo o prompt para o consultor de montanha
+prompt_consultor_montanha = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Apresente-se como Sr. Montanha. Você é um consultor de viagens especialista em destinos de montanha"),
+        ("human", "{query}"),
+    ],
+)
+
+cadeia_praia = prompt_consultor_praia | modelo | StrOutputParser()
+cadeia_montanha = prompt_consultor_montanha | modelo | StrOutputParser()
+
+
+class Rota(TypedDict):
+    destino: Literal["praia", "montanha"]
+
+prompt_roteador = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Responda apenas com 'praia' ou 'montanha'"),
+        ("human", "{query}"),
+    ],
+)
+
+# Definindo o roteador que decide qual consultor usar com base na consulta do usuário
+roteador = prompt_roteador | modelo.with_structured_output(Rota)
+
+# Função para processar a consulta do usuário e direcioná-la ao consultor apropriado, sem usar o LangGraph
+def response(query: str) -> str:
+    rota = roteador.invoke({"query": query})["destino"]
+    if rota == "praia":
+        return cadeia_praia.invoke({"query": query})
+    else:
+        return cadeia_montanha.invoke({"query": query})
+    
+print(response("Quero surfar em um lugar de águas cristalinas."))
